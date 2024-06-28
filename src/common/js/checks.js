ef5813bfc1558a558fd2502816da095f2d059103
Pierre Wieser <p.wieser@trychlos.org>
2024-06-28 02:03:35 +0200

    Check the start and end dates

---
+++ src/common/js/checks.js
@@ -0,0 +1,56 @@
+/*
+ * pwix:validity/src/common/js/checks.js
+ */
+
+import _ from 'lodash';
+const assert = require( 'assert' ).strict;
+
+import { DateJs } from 'meteor/pwix:date';
+import { pwixI18n } from 'meteor/pwix:i18n';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { TM } from 'meteor/pwix:typed-message';
+
+Validity.checks = {
+    // item is a ReactiveVar which contains the edited document
+    _assert_data_itemrv( caller, data ){
+        assert.ok( data, caller+' data required' );
+        assert.ok( data.item, caller+' data.item required' );
+        assert.ok( data.item instanceof ReactiveVar, caller+' data.item expected to be a ReactiveVar' );
+    },
+
+    // if date is set, it must be valid - it is expected in yyyy-mm-dd format
+    //  data comes from the edition panel, passed-in through the Forms.Checker instance
+    async effectEnd( value, data, opts={} ){
+        this._assert_data_edited( 'Validity.checks.effectEnd()', data );
+        const item = data.item.get();
+        return Promise.resolve( null )
+            .then(() => {
+                if( opts.update !== false ){
+                    item.effectEnd = value ? new Date( value ) : null;
+                    data.item.set( item );
+                }
+                const msg = Validity.checkEnd( edited, data.item.get());
+                return msg ? new TM.TypedMessage({
+                    type: opts.MessageType.C.ERROR,
+                    message: msg
+                }) : null;
+            });
+    },
+
+    async effectStart( value, data, opts={} ){
+        this._assert_data_edited( 'Validity.checks.effectStart()', data );
+        const edited = data.edited.get();
+        return Promise.resolve( null )
+            .then(() => {
+                if( opts.update !== false ){
+                    data.item.effectStart = value ? new Date( value ) : null;
+                    data.edited.set( edited );
+                }
+                const msg = Meteor.APP.Validity.checkStart( edited, data.item.get());
+                return msg ? new opts.TypedMessage({
+                    type: opts.MessageType.C.ERROR,
+                    message: msg
+                }) : null;
+            });
+    }
+};
