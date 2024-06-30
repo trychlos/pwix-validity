803170278289c0dd34143c9bac4e20223c8d342f
Pierre Wieser <p.wieser@trychlos.org>
2024-06-30 21:57:07 +0200

    Fieldset has to be included into tabular data

--- src/common/js/records-fieldset.js
+++ src/common/js/records-fieldset.js
@@ -10,14 +10,14 @@ Validity.recordsFieldDef = function(){
         {
             name: 'entity',
             type: String,
-            dt_tabular: false,
+            dt_visible: false,
             help_line: pwixI18n.label( I18N, 'help.entity_line' )
         },
         {
             name: 'effectStart',
             type: Date,
             optional: true,
-            dt_tabular: false,
+            dt_visible: false,
             form_check: Validity.checks.effectStart,
             form_status: Forms.FieldType.C.OPTIONAL,
             help_line: pwixI18n.label( I18N, 'help.start_line' )
@@ -26,7 +26,7 @@ Validity.recordsFieldDef = function(){
             name: 'effectEnd',
             type: Date,
             optional: true,
-            dt_tabular: false,
+            dt_visible: false,
             form_check: Validity.checks.effectEnd,
             form_status: Forms.FieldType.C.OPTIONAL,
             help_line: pwixI18n.label( I18N, 'help.end_line' )
